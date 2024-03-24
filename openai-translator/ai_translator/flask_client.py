b33127e77b21ea597fb422cdd98a4db04960dae7
doubleTao <daniel.h.leo165@gmail.com>
2024-03-24 10:00:46 +0800

    homework0324

--- openai-translator/ai_translator/flask_client.py
+++ openai-translator/ai_translator/flask_client.py
@@ -4,9 +4,9 @@ from utils import LOG
 
 url = 'http://127.0.0.1:8000/translate'
 
-data = {'data': 'doubleTao'}
+author = {'author': 'doubleTao'}
 files = {'file': 'test.pdf'} # open('flask_client.py', 'rb')
 
-res = requests.post(url=url, data=data, files=files)
+res = requests.post(url=url, data=author, files=files)
 
 LOG.debug(f"响应: {res}\n")
